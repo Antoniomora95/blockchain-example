6d75b75f28b6b093c793277e7355491e46858f0d
amora <antonio.mora@oracle.com>
2021-11-14 16:29:30 -0600

    use the existing backend to test a form submit functionality, looks awesome

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
 var app = express();
+var cors = require('cors');
+
+app.use(cors())
 
 app.get('/', function (req, res) {
     res.send('hello world.');
+});
+
+app.post('/user', function (req, res) {
+    let dateStr = new Date().toLocaleString();
+    res.send('user was created at: '+ dateStr);
 });
 
 app.listen(3000, function () {
