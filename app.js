4cf2a2788746bbae70cc4c1e1f70ab6cdd8122e6
amora <antonio.mora@oracle.com>
2021-11-14 16:49:53 -0600

    add body parse to be able to read body coming on request

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
 var cors = require('cors');
+
+
+// investigate why do we need body parser --> i installed it because all the body coming in req was undefined
+// but need to know why.
+// also check how does cors work, without line 12, requests will fail in your client app, index.html in this case
+app.use(bodyParser.json());
 
 app.use(cors())
 
@@ -11,7 +17,9 @@ app.get('/', function (req, res) {
 
 app.post('/user', function (req, res) {
     let dateStr = new Date().toLocaleString();
-    res.send('user was created at: '+ dateStr);
+    console.log('backend', req);
+    response = {message: 'user was created at: '+ dateStr, data: req.body};
+    res.send(response);
 });
 
 app.listen(3000, function () {
